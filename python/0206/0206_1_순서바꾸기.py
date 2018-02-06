400434fd911bd900b11780f749fb64d6da3d70eb
dubugun <sien0104@gmail.com>
2018-02-06 16:29:06 +0900

    0206/16:30

---
+++ python/0206/0206_1_순서바꾸기.py
@@ -0,0 +1,6 @@
+def revr(a,b,c) :
+    aa = []
+    aa.append(c)
+    aa.append(b)
+    aa.append(a)
+    print(aa)
---
+++ python/0206/바운더리정규식.py
@@ -0,0 +1,6 @@
+import re
+
+a = 'Raindrops# on Roses*, and Whiskers! on Kittens.'
+
+r = re.compile(r'\b[\w]*[\W]\B')
+
